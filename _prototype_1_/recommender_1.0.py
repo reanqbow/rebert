90d744f8e91546998f98f89cf01cff13ad0fddf4
Yubo Zhao <reanqbow@gmail.com>
2025-04-04 15:34:22 -0700

    initial commit

---
+++ _prototype_1_/recommender_1.0.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# 
+#   FILE: recommender_1.0.py
+#   REVISION: March, 2024
+#   CREATION DATE: March, 2024
+#   AUTHOR: David W. McDonald
+#
+#   Prototype: 1
+#   Version: 0
+#
+#   This is the code for a very simple chat prototype of a movie recommender. This example is
+#   meant to be minimalistic to help you understand how a chat type AI system might work
+#   
+#   Copyright by Author. All rights reserved. Not for reuse without express permissions.
+#
+
+#   These are standard python modules/packages
+import sys, datetime, json
+import os
+#
+#   You may need to install this one - depending on what kind of python you are using
+try:
+    import requests
+except:
+    print("\n\nThis example depends upon the 'requests' module. This exception is because ")
+    print("it looks like you have not installed the 'requests' module. You should visit")
+    print("     https://pypi.org/project/requests/")
+    print("for instructions on installing 'requests'.\n\n")
+    sys.exit()
+
+
+#   CONSTANTS
+#
+#   This example will use an OpenAI service
+OAI_HOST = "https://api.openai.com"
+OAI_SERVICE_ENDPOINT = "/v1/chat/completions"
+OAI_MODEL = "gpt-4-turbo-preview"
+#
+#   One should never put their key right in the code like this
+#   A later prototype will show an alternative that solves this problem
+API_KEY = os.environ["OPENAI_API_KEY"]
+
+#
+#   Create a new chat turn.
+#   Each chat turn is a dictionary with a 'role' and 'content'
+def new_chat_turn(role="",content=""):
+    turn = dict()
+    turn['role'] = role
+    turn['content'] = content
+    return turn
+#
+#   The code needs to maintain the status of the chat. This status 
+#   will include parameters that tell the model how it should respond
+#   as well as all of the user questions and the responses.
+def new_chat_context():
+    chat_context = dict()
+    chat_context['model'] = OAI_MODEL
+    chat_context['messages'] = list()
+    return chat_context
+#
+#   Making a request is about modifying the growing chat_context
+#   setting up the HTTP request URL and request headers, and making
+#   the request.
+def make_chat_request(user_text="", chat_context=None, api_key=""):
+    #   If no chat context is provided, then this is a new chat
+    if not chat_context:
+        # create and use a new chat context
+        chat_context = new_chat_context()
+    
+    #   We use the text we got from the user to create a user turn
+    user_turn = new_chat_turn("user",user_text)
+    #   Add that user turn to the list of messages in the context
+    chat_context['messages'].append(user_turn)
+    
+    #   The whole context is payload for the request - a request body
+    payload = json.dumps(chat_context)
+    
+    #   Create header information for the request - this must include
+    #   the 'Content-Type' key set to 'application/json' and the
+    #   'Authorization' key set to include your API key
+    header = dict()
+    header['Content-Type'] = "application/json"
+    header['Authorization'] = f"Bearer {api_key}"
+    
+    #   The service URL is the host and the service endpoint
+    service_url = OAI_HOST + OAI_SERVICE_ENDPOINT
+    
+    #   Make this as a POST request
+    response = requests.post(service_url,
+                             headers=header,
+                             data=payload)
+    #   The response should be 'application/json' so extract the JSON
+    resp_dict = response.json()
+    #   There is a lot in the response - just extract the message
+    # print(resp_dict)
+    assistant_turn = resp_dict['choices'][0]['message']
+    #   Add the response to our chat context
+    chat_context['messages'].append(assistant_turn)
+    return chat_context
+
+#
+#   The main is called from the command line and just loops asking
+#   for user to input
+def main():
+    #   Initialize some variables
+    chat_context = None
+    assistant_name = sys.argv[0].rpartition('.')[0]
+    print()
+    
+    #   A rather simple chat loop
+    user_text = input(f"You > ").strip()
+    print()
+    #   While the user enters some text - not 'quit'
+    while len(user_text)>0 and (user_text.lower() != "quit"):
+        
+        #   Use that user text to make the request
+        chat_context = make_chat_request(user_text, chat_context, API_KEY)
+        
+        #   Get the last message - it should be the text response
+        assistant_turn = chat_context['messages'][-1]
+        
+        #   Show that response
+        print(f"{assistant_name} > {assistant_turn['content']}")
+        print()
+        
+        #   Get the next user turn
+        user_text = input(f"You > ").strip()
+        print()
+    
+    return
+
+if __name__ == '__main__':
+    main()   
+
+
+
---
+++ _prototype_1_/recommender_1.2.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# 
+#   FILE: recommender_1.2.py
+#   REVISION: March, 2024
+#   CREATION DATE: March, 2024
+#   AUTHOR: David W. McDonald
+#
+#   Prototype: 1
+#   Version: 2
+#
+#   This is the code for a very simple chat prototype of a movie recommender. The primary enhancement
+#   for this version is code that allows us to view the fields of our request body (the payload) and
+#   to see the full content of the response from the server. As you ask questions the size of the
+#   request body (payload) will grow. This gives the remote server all of the context of the chat so
+#   it can respond knowing what conversation has happend before.
+#   
+#   Copyright by Author. All rights reserved. Not for reuse without express permissions.
+#
+
+#   These are standard python modules/packages
+import sys, datetime, json
+#
+#   You may need to install this one - depending on what kind of python you are using
+try:
+    import requests
+except:
+    print("\n\nThis example depends upon the 'requests' module. This exception is because ")
+    print("it looks like you have not installed the 'requests' module. You should visit")
+    print("     https://pypi.org/project/requests/")
+    print("for instructions on installing 'requests'.\n\n")
+    sys.exit()
+
+#   CONSTANTS
+#
+#   This example will use an OpenAI service
+OAI_HOST = "https://api.openai.com"
+OAI_SERVICE_ENDPOINT = "/v1/chat/completions"
+OAI_MODEL = "gpt-4-turbo-preview"
+#
+#   One should never put their key right in the code like this
+#   A later prototype will show an alternative that solves this problem
+API_KEY = "<GET_AN_API_KEY_AND_PUT_IT_HERE"
+#
+#   Depending on what model and what service you are using, the prior 
+#   version (p.v:1.0) might not make any movie recommendation. One way to 
+#   fix that is with the use of a 'system' message. A 'system' turn is one 
+#   way to create a persona for the assistant that responds to the user.
+#   Here we will set up a very simple movie recommender persona and try to
+#   get the assistant to always respond with a movie.
+#
+MOVIE_RECOMMENDER_PERSONA_PROMPT = '''You are a movie critic who wants to make sure that you make the best movie recommendations. Make sure that the movie you recommend satisfies the user across many movie attributes including genre, actors, visuals, music, plot line, character development, dialog, mood, and many other movie attributes. Your responses should always focus on making movie recommendations.'''
+
+
+#
+#   Create a new chat turn.
+#   Each chat turn is a dictionary with a 'role' and 'content'
+def new_chat_turn(role="",content=""):
+    turn = dict()
+    turn['role'] = role
+    turn['content'] = content
+    return turn
+#
+#   The code needs to maintain the status of the chat. This status 
+#   will include parameters that tell the model how it should respond
+#   as well as all of the user questions and the responses.
+def new_chat_context():
+    chat_context = dict()
+    chat_context['model'] = OAI_MODEL
+    chat_context['messages'] = list()
+    system_turn = new_chat_turn("system",MOVIE_RECOMMENDER_PERSONA_PROMPT)
+    chat_context['messages'].append(system_turn)
+    return chat_context
+#
+#   Making a request is about modifying the growing chat_context
+#   setting up the HTTP request URL and request headers, and making
+#   the request.
+def make_chat_request(user_text="", chat_context=None, api_key=""):
+    #   If no chat context is provided, then this is a new chat
+    if not chat_context:
+        # create and use a new chat context
+        chat_context = new_chat_context()
+    
+    #   We use the text we got from the user to create a user turn
+    user_turn = new_chat_turn("user",user_text)
+    #   Add that user turn to the list of messages in the context
+    chat_context['messages'].append(user_turn)
+    
+    #   The whole context is payload for the request - a request body
+    payload = json.dumps(chat_context)
+    
+    #   Create header information for the request - this must include
+    #   the 'Content-Type' key set to 'application/json' and the
+    #   'Authorization' key set to include your API key
+    header = dict()
+    header['Content-Type'] = "application/json"
+    header['Authorization'] = f"Bearer {api_key}"
+    
+    #   The service URL is the host and the service endpoint
+    service_url = OAI_HOST + OAI_SERVICE_ENDPOINT
+    
+    #   Make this as a POST request
+    response = requests.post(service_url,
+                             headers=header,
+                             data=payload)
+    #   The response should be 'application/json' so extract the JSON
+    resp_dict = response.json()
+    print()
+    print("REQUEST BODY =================================")
+    print(json.dumps(chat_context, indent=4))
+    print("RESPONSE =====================================")
+    print(json.dumps(resp_dict, indent=4))
+    print("==============================================")
+    print()
+    #   There is a lot in the response - just extract the message
+    assistant_turn = resp_dict['choices'][0]['message']
+    #   Add the response to our chat context
+    chat_context['messages'].append(assistant_turn)
+    return chat_context
+
+#
+#   The main is called from the command line and just loops asking
+#   for user to input
+def main():
+    #   Initialize some variables
+    chat_context = None
+    assistant_name = sys.argv[0].rpartition('.')[0]
+    print()
+    
+    #   A rather simple chat loop
+    user_text = input(f"You > ").strip()
+    print()
+    #   While the user enters some text - not 'quit'
+    while len(user_text)>0 and (user_text.lower() != "quit"):
+        
+        #   Use that user text to make the request
+        chat_context = make_chat_request(user_text, chat_context, API_KEY)
+        
+        #   Get the last message - it should be the text response
+        assistant_turn = chat_context['messages'][-1]
+        
+        #   Show that response
+        print(f"{assistant_name} > {assistant_turn['content']}")
+        print()
+        
+        #   Get the next user turn
+        user_text = input(f"You > ").strip()
+        print()
+    
+    return
+
+if __name__ == '__main__':
+    main()   
+
+
+
